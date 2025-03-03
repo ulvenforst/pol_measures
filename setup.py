2dc1ae40e31aa5be98f7b4c93eec464c148f8a59
Juan Camilo Narváez <66099723+Ulvenforst@users.noreply.github.com>
2025-03-03 14:51:50 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="measures",
-    version="0.1.0a1",
+    version="0.1.0a2",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
